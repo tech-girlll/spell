9944e06b831b4ea21aff0c102a199c5a875e95c0
NnannaMari09033 <nnannamari@gmail.com>
2026-05-22 14:34:36 -0400

    spinning up things

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def signup(request):
+    """Create a new user account, then log them in."""
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/")
+    else:
+        form = UserCreationForm()
+    return render(request, "accounts/signup.html", {"form": form})
